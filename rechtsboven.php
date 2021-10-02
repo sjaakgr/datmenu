8089f1d274ec4aa7a34e75617f67e07ab371d03d
sjaakgr <sjaak@webkamer.nl>
2021-10-02 16:44:16 +0200

    Nieuwe poging

--- rechtsboven.php
+++ rechtsboven.php
@@ -1,3 +1,11 @@
-<?php 
-    echo "jaja" . "<BR>";
+<?php
+echo '<html>';
+echo '<body>';
+
+echo '<h1>My First Heading</h1>';
+
+echo '<p>My first paragraph.</p>';
+
+echo '</body>';
+echo '</html>';
 ?>
